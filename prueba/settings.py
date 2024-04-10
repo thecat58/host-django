71822af45dbda6243fc66c5552891267dd49f84a
thecat58 <mkike518@gmail.com>
2024-04-09 20:39:42 -0500

    host

--- prueba/settings.py
+++ prueba/settings.py
@@ -125,4 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'principal/static')
 ]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['host-django-production.up.railway.app']
